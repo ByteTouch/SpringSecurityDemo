e5dc5d613c949290cf38907d5876050eb5865412
Wan Cheng <j18467tw@edu.tuis.ac.jp>
2021-06-01 19:12:31 +0900

    test

---
+++ src/main/java/com/example/springsecurity/security/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.example.springsecurity.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationProviderService authenticationProvider;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(authenticationProvider);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+            .defaultSuccessUrl("/hello", true);
+
+        http.authorizeRequests()
+            .anyRequest().authenticated();
+    }
+}
